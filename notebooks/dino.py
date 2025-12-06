2626b950db06c6a15a98f003ee62f71f281f12a6
Tripp Lyons <tripplyons@gmail.com>
2025-12-06 12:41:30 -0500

    initial dino setup

---
+++ notebooks/dino.py
@@ -0,0 +1,215 @@
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from transformers import AutoModel, pipeline, AutoImageProcessor
+from transformers.image_utils import load_image
+import numpy as np
+import io
+from PIL import Image
+import torch
+import torch.nn as nn
+
+url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/pipeline-cat-chonk.jpeg"
+image = load_image(url)
+
+model_id = "facebook/dinov3-convnext-tiny-pretrain-lvd1689m"
+
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
+
+
+def get_feature_extractor(model_id, device):
+    torch.set_default_device(device)
+    image_processor = AutoImageProcessor.from_pretrained(model_id, use_fast=True)
+    model = AutoModel.from_pretrained(model_id).to(device)
+    return pipeline(
+        model=model,
+        task="image-feature-extraction",
+        image_processor=image_processor,
+        use_fast=True,
+        framework="pt",
+        device_map="auto",
+    )
+
+
+def get_features(feature_extractor, images, device):
+    features = feature_extractor(
+        images,
+        batch_size=len(images),
+        return_tensors="pt",
+        device=device,
+    )
+    features = (
+        torch.concat(features, dim=0).mean(-2).to(device)
+    )  # average pooling over tokens
+    return features.flatten(start_dim=1)
+
+
+def load_dataset_df():
+    import os
+
+    os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
+    from datasets import load_dataset
+
+    dataset_id = "imageomics/invasive_plants_hawaii"
+
+    # splits are "dorsal", "ventral", "both"
+    both_dataset = load_dataset(dataset_id, split="both")
+    both_df = both_dataset.to_pandas()
+    del both_dataset
+    return both_df
+
+
+class PlantsDataloader:
+    def __init__(self, df, feature_extractor, device, batch_size=64):
+        self.device = device
+        self.df = df
+        self.damage_types = [
+            "healthy",
+            "rust",
+            "leaf_miner",
+            "other_insect",
+            "mechanical_damage",
+        ]
+        self.batch_size = batch_size
+        self.feature_extractor = feature_extractor
+
+    def binary_string_to_image(self, binary_string) -> torch.Tensor:
+        file = io.BytesIO(binary_string)
+        return Image.open(file)
+
+    def epoch_iterator(self, shuffle=True):
+        if shuffle:
+            order = np.random.permutation(range(len(self.df)))
+        else:
+            order = range(len(self.df))
+        batch_images = []
+        batch_labels = []
+        for idx in order:
+            row = self.df.iloc[idx]
+            image = self.binary_string_to_image(row["image"]["bytes"])
+            batch_images.append(image)
+            batch_labels.append(
+                torch.tensor(
+                    row[self.damage_types]
+                    .apply(lambda x: x == "Yes")
+                    .values.astype(np.float32),
+                    device=self.device,
+                )
+            )
+            if len(batch_images) == self.batch_size:
+                yield (
+                    get_features(self.feature_extractor, batch_images, self.device),
+                    torch.stack(batch_labels),
+                )
+                batch_images = []
+                batch_labels = []
+        if len(batch_images) > 0:
+            yield (
+                get_features(self.feature_extractor, batch_images, self.device),
+                torch.stack(batch_labels),
+            )
+
+
+def main():
+    seed = 0
+    batch_size = 1
+    learning_rate = 3e-4
+    device = get_device()
+
+    both_df = load_dataset_df()
+    train_df, val_df = train_test_split(both_df, test_size=0.1, random_state=seed)
+
+    feature_extractor = get_feature_extractor(model_id, device)
+    train_dataloader = PlantsDataloader(
+        train_df, feature_extractor, device, batch_size=batch_size
+    )
+    val_dataloader = PlantsDataloader(
+        val_df, feature_extractor, device, batch_size=batch_size
+    )
+
+    model = nn.Linear(768, 5).to(device)
+
+    model.train()
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    for features, labels in tqdm(
+        train_dataloader.epoch_iterator(shuffle=True), desc="Training"
+    ):
+        optimizer.zero_grad()
+        logits = model(features)
+        loss = loss_fn(logits, labels)
+        loss.backward()
+        optimizer.step()
+        print(f"Loss: {loss.item():.4f}")
+
+    model.eval()
+
+    all_predictions = []
+    all_labels = []
+    with torch.no_grad():
+        for features, labels in tqdm(
+            val_dataloader.epoch_iterator(shuffle=False), desc="Evaluating"
+        ):
+            logits = model(features)
+            all_predictions.append(torch.sigmoid(logits).cpu())
+            all_labels.append(labels.cpu())
+        all_predictions = torch.cat(all_predictions, dim=0)
+        all_labels = torch.cat(all_labels, dim=0)
+        all_labels = all_labels.int()
+
+        max_f1s = []
+        # roc_aucs = []
+        for i in range(5):
+            thresholds = np.linspace(0, 1, 100)[1:-1]
+            precision = []
+            recall = []
+            f1 = []
+            for threshold in thresholds:
+                preds = (all_predictions[:, i] > threshold).int()
+                predicted_positive = preds == 1
+                true_positive = all_labels[:, i] == 1
+                if predicted_positive.sum() == 0:
+                    precision.append(1)
+                else:
+                    precision.append(
+                        (
+                            (preds == all_labels[:, i])[predicted_positive].int().sum(0)
+                            / predicted_positive.sum()
+                        )
+                        .cpu()
+                        .item()
+                    )
+                if true_positive.sum() == 0:
+                    recall.append(1)
+                else:
+                    recall.append(
+                        (
+                            (preds == all_labels[:, i])[true_positive].int().sum(0)
+                            / true_positive.sum()
+                        )
+                        .cpu()
+                        .item()
+                    )
+                p = precision[-1]
+                r = recall[-1]
+                print(f"Precision: {p}, Recall: {r}")
+                f1.append(2 * p * r / (p + r))
+            max_f1s.append(max(f1))
+            # fpr_curve, tpr_curve, _ = sklearn.metrics.roc_curve(
+            #     all_labels_np[:, i], all_predictions_np[:, i]
+            # )
+            # roc_aucs.append(sklearn.metrics.auc(fpr_curve, tpr_curve))
+        print(f"Damage types: {train_dataloader.damage_types}")
+        print(f"Max F1s: {max_f1s}")
+        # print(f"ROC AUCs: {roc_aucs}")
+
+
+if __name__ == "__main__":
+    main()
